a22daeabcce3922131ec4adf8a75d54c4bf9d29c
valenlyn <valenlynchua@gmail.com>
2019-05-30 08:46:48 +0800

    Changes to copy

--- src/client/components/index/description/description.jsx
+++ src/client/components/index/description/description.jsx
@@ -9,6 +9,7 @@ class Form extends React.Component {
         return (
             <div className={styles.descWrapper}>
 
+                <h1>Make better decisions together</h1>
                 <p className={styles.descText} >
                     <i className="material-icons">check_circle</i> Find out what your friends <span class="italic">TRULY want</span> <br />
                     <i className="material-icons">check_circle</i> Safe space to indicate preferences—<span class="italic">dissent MADLY and anonymously!</span> <br />
@@ -18,6 +19,9 @@ class Form extends React.Component {
                     <h1>What is the Abilene paradox?</h1>
                     <p className={styles.descText}>
                     In the Abilene paradox, a group of people collectively decide on a course of action that is counter to the preferences of many or all of the individuals in the group. It involves a common breakdown of group communication in which each member mistakenly believes that their own preferences are counter to the group's and, therefore, does not raise objections.
+                    <br /><br />
+                    <span class={styles.tinyText}>Source: <a href="https://en.wikipedia.org/wiki/Abilene_paradox" target="_blank" rel="noopener noreferrer">Wikipedia</a>
+                    </span>
                     </p>
                 </div>
             </div>
