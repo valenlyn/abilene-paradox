fbd9f4a98433d43625c2e84294380b745ba4157c
valenlyn <valenlynchua@gmail.com>
2019-05-30 14:36:19 +0800

    uglify

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,6 +1,6 @@
 const {resolve} = require('path');
 const merge = require('webpack-merge');
-const TerserPlugin = require('terser-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const commonConfig = require('./webpack.config.common');
@@ -8,9 +8,10 @@ const commonConfig = require('./webpack.config.common');
 module.exports = merge(commonConfig, {
   mode: 'production',
   plugins: [
-  optimization: {
-    minimizer: [new TerserPlugin()],
-  },
+    new UglifyJsPlugin({
+      parallel: true,
+      extractComments: true
+    }),
     new HtmlWebpackPlugin({
       hash: true,
       inject: true,
